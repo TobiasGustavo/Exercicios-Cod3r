c91f89222777a54687566ca920698c337d364e89
tgzss <tobiasgustavoh@gmail.com>
2022-05-21 21:12:22 -0300

    Issue #1 .NET
    C#

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+	 class Comentarios
+	{
+		/// <summary>
+        /// CTRL+space - demonstra outras opções
+        /// </summary>
+		public static void Executar(){
+			//Comentarios simples
+			Console.WriteLine("Código claro é sempre melhor");
+			
+			Console.WriteLine("O C# tem o XML Comments");
+		}
+	}
+}
+
